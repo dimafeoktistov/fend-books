7efcf59aa520fb8d84078f5397f843d5ec69e9e5
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-04-11 16:15:30 +0700

    Trying stuff

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,3 +1,67 @@
-const books = [];
-
-export default BooksList;
+const booksList = [
+  {
+    bookshelf: 'currentlyReading',
+    id: '_oG_iTxP1pIC',
+    title: 'Flowers for Algernon',
+    authors: ['Daniel Keyes'],
+    thumbnail:
+      'http://books.google.com/books/content?id=_oG_iTxP1pIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    bookshelf: 'currentlyReading',
+    id: 'Kz0vseLs4JcC',
+    title: 'The Grizzly King',
+    authors: ['James Oliver Curwood'],
+    thumbnail:
+      'http://books.google.com/books/content?id=Kz0vseLs4JcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    bookshelf: 'wantToRead',
+    id: 's-QzccStux4C',
+    title: 'Blood Meridian',
+    authors: ['Cormac McCarthy'],
+    thumbnail:
+      'http://books.google.com/books/content?id=s-QzccStux4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    bookshelf: 'wantToRead',
+    id: 'bIOu6k8a1C4C',
+    title: 'Last Exit to Brooklyn',
+    authors: ['Hubert Selby'],
+    thumbnail:
+      'http://books.google.com/books/content?id=bIOu6k8a1C4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    bookshelf: 'wantToRead',
+    id: 'Igx0aWvzc34C',
+    title: 'All quiet on the western front and related readings',
+    authors: ['Erich Maria Remarque'],
+    thumbnail:
+      'http://books.google.com/books/content?id=Igx0aWvzc34C&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  },
+  {
+    bookshelf: 'read',
+    id: 'wLQrDwAAQBAJ',
+    title: 'Love of Life & Other Stories',
+    authors: ['Jack London'],
+    thumbnail:
+      'http://books.google.com/books/content?id=wLQrDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    bookshelf: 'read',
+    id: 'LLWMswEACAAJ',
+    title: 'The good soldier Švejk and his fortunes in the World War',
+    authors: ['Jaroslav Hašek'],
+    thumbnail:
+      'http://books.google.com/books/content?id=LLWMswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  },
+  {
+    bookshelf: 'read',
+    id: 'ChlOAwAAQBAJ',
+    title: 'The Name of the Rose',
+    authors: ['Umberto Eco'],
+    thumbnail:
+      'http://books.google.com/books/content?id=ChlOAwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  }
+];
+export default booksList;
--- src/components/ShelfBox.js
+++ src/components/ShelfBox.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import Shelf from './Shelf.js';
+import booksList from './BooksList.js';
 
 class ShelfBox extends Component {
-  render() {}
+  state = {
+    listOfBooks: booksList,
+    listOfShelfes: ['Bla-Bla', 'Currently reading', 'Read']
+  };
+  render() {
+    return (
+      <div className="list-books-content">
+        <Shelf titles={this.state.listOfShelfes} />
+      </div>
+    );
+  }
 }
 
 export default ShelfBox;
